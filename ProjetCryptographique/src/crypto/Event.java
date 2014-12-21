4e61b88e4f0c82597d2eac32c81eda8f3c9f68f4
DaftSpirit <joris.rat@gmail.com>
2014-12-21 09:09:36 +0100

    added default constructor and 1 baluate constructor for debug and tests

--- ProjetCryptographique/src/crypto/Event.java
+++ ProjetCryptographique/src/crypto/Event.java
@@ -1,5 +1,37 @@
 package crypto;
 
+import java.util.Date;
+
 public class Event {
+	
+	private String name;
+	private Date date;
+	private boolean crypted;
+	
+	/**
+	 * 	public Event()
+	 * 	default constructor for non crypted events
+	 * 	@param null
+	 */
+	public Event()
+	{
+		this.name = "default event";
+		this.crypted = false;
+		this.date = new Date(2015, 1, 1);
+	}
+	
+	/**
+	 * public Event(boolean)
+	 * debug constructor for tests and debug
+	 * @param crypted defines if the event is crypted or not
+	 */
+	public Event(boolean crypted)
+	{
+		this.name = "debug event";
+		this.date = new Date(2015, 1, 1);
+		this.crypted = crypted;
+	}
+	
+	
 
 }
