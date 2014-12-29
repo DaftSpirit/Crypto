1793933c059584423f01813b749b17cff041d1c3
DaftSpirit <joris.rat@gmail.com>
2014-12-29 20:02:05 +0100

    change of main to Main unless a main class is weird

--- ProjetCryptographique/src/crypto/Main.java
+++ ProjetCryptographique/src/crypto/Main.java
@@ -4,8 +4,9 @@ import intergraphique.Interface_P;
 
 /* Une classe main pour tester
  * auteur: Axel
+ * srsly une classe qui s'appelle main ?! je rajoute la majuscule au début du nom
  */
-public class main {
+public class Main {
 
 	public static void main(String[] args) {
 		Interface_P i1 = new Interface_P();
