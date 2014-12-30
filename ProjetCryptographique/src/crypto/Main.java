5c009de9fae42ce314b7018c4fd610d9bedbd3dc
Axel <axeljacki@gmail.com>
2014-12-30 01:06:34 +0100

    main

--- ProjetCryptographique/src/crypto/Main.java
+++ ProjetCryptographique/src/crypto/Main.java
@@ -5,7 +5,9 @@ import intergraphique.Interface_P;
 /* Une classe main pour tester
  * auteur: Axel
  * srsly une classe qui s'appelle main ?! je rajoute la majuscule au début du nom -> Joris
+ * Ouep c'est pour tester si mon interface graphique fonctionne bien. Mais je vois pas ou est le pb avec le nom
  */
+
 public class Main {
 
 	public static void main(String[] args) {
