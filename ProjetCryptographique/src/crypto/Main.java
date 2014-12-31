bcc8e8f09949db5ab226cbcd500135159628795a
Axel <axeljacki@gmail.com>
2014-12-31 15:34:47 +0100

    main

--- ProjetCryptographique/src/crypto/Main.java
+++ ProjetCryptographique/src/crypto/Main.java
@@ -2,7 +2,9 @@ package crypto;
 
 import intergraphique.Interface_P;
 
-/* 
+/* Une classe main pour tester
+ * auteur: Axel
+ * srsly une classe qui s'appelle main ?! je rajoute la majuscule au début du nom -> Joris 
  */
 
 public class Main {
