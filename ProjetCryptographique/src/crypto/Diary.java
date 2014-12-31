b1d7627dc049dfddebc9d35187db8cd01a5ab14e
DaftSpirit <joris.rat@gmail.com>
2014-12-31 16:11:33 +0100

    Added the data member key for the class Diary.

--- ProjetCryptographique/src/crypto/Diary.java
+++ ProjetCryptographique/src/crypto/Diary.java
@@ -2,14 +2,18 @@ package crypto;
 
 import java.util.Map;
 
+import javax.crypto.spec.SecretKeySpec;
+
 /**
  * 
  * events : map of all events the key is the name of the event
+ * key : SecretKeySpec generated for the password of the user. used to test if the password is correct
  * @author joris
  *
  */
 public class Diary {
 	
 	private Map<String,Event> events;
+	private SecretKeySpec key;
 
 }
