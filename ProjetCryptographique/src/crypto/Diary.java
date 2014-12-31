ff41ae631ee07057ec5dd8c02fda226095fe3294
DaftSpirit <joris.rat@gmail.com>
2014-12-31 15:54:11 +0100

    Added Javadoc for class Diary

--- ProjetCryptographique/src/crypto/Diary.java
+++ ProjetCryptographique/src/crypto/Diary.java
@@ -2,7 +2,12 @@ package crypto;
 
 import java.util.Map;
 
-
+/**
+ * 
+ * events : map of all events the key is the name of the event
+ * @author joris
+ *
+ */
 public class Diary {
 	
 	private Map<String,Event> events;
