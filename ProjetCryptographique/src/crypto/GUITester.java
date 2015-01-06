4816723332d01486880bfc72c96a72d26aa9d26f
gryffenferd <axeljacki@gmail.com>
2015-01-06 17:39:52 +0100

    le meme que avant windows fonctionnelle

--- ProjetCryptographique/src/crypto/GUITester.java
+++ ProjetCryptographique/src/crypto/GUITester.java
@@ -3,14 +3,13 @@ package crypto;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Date;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 
 import gui.GUIContainer;
-
-/* Une classe main pour tester */
 
 public class GUITester {
 
@@ -19,13 +18,17 @@ public class GUITester {
 			IllegalBlockSizeException, BadPaddingException,
 			InvalidAlgorithmParameterException {
 		// Interface_P i1 = new Interface_P();
-		Event e1 = new Event();
+		Date d1 = new Date();
+		Event e1 = new Event("Event 1", "une description", d1, false);
+		System.out.println(e1.toString());
+		e1.encrypt();
 		System.out.println(e1);
-		/*e1.encrypt();
-		System.out.println(e1);
-		e1.decrypt();
+		/*e1.decrypt();
 		System.out.println(e1);*/
 
+		/*Crypter cr = new Crypter("popoyhyyyyyyyy");
+		System.out.println(cr.decrypt(cr.encrypt("Taiii")));*/
 	}
+	
 
 }
