a3b115011d4e9d3e2b6b75c78e7e8a82071b3176
gael <gael@Sanji-San>
2015-01-09 10:50:54 +0100

    Modification de la méthode de lecture et d'écriture. 	*Ecriture* : Ajout du nombre d'objet à extraire du fichier. Permet d'éviter le EOFException 	*Lecture* : suppression de l'itérateur au profit d'une boucle for(). On n'essaye plus de lire après la fin du fichier

--- ProjetCryptographique/src/crypto/GUITester.java
+++ ProjetCryptographique/src/crypto/GUITester.java
@@ -21,79 +21,76 @@ import javax.crypto.NoSuchPaddingException;
 
 public class GUITester {
 	public static void main(String[] args) {
-		
+
 		Crypter cr;
 		try {
 			ArrayList<AbsEvent> diary = new ArrayList<AbsEvent>();
 			cr = new Crypter("password");
-			
+
 			Date d1 = new Date();
 			Event e1 = new Event("Event 1", "une description", d1);
-			
+
 			diary.add(e1);
-			//System.out.println(e1);
-			
+			// System.out.println(e1);
+
 			EventCrypted ce;
 			ce = cr.encryptEvent(e1);
-			//System.out.println(ce);
+			// System.out.println(ce);
 			diary.add(ce);
-			
-			//System.out.println(ce);
+
+			// System.out.println(ce);
 			Event e2 = cr.decryptEvent(ce);
 			System.out.println(e2);
 
-			/* Writing in a file the diary */		
+			/* Writing in a file the diary */
+//			ObjectOutputStream oos = new ObjectOutputStream(
+//					new BufferedOutputStream(new FileOutputStream(new File(
+//							"agenda.txt"))));
+//			Iterator<AbsEvent> it = diary.iterator();
+//			oos.writeInt(diary.size());		//writing number of event to read used for reading
+//			while (it.hasNext()) {
+//				oos.writeObject(it.next());
+//			}
+//			oos.close();
 
-			ObjectOutputStream oos = new ObjectOutputStream(
-					new BufferedOutputStream(
-							new FileOutputStream(new File("agenda.txt"))));
-			Iterator<AbsEvent> it = diary.iterator();
-			while(it.hasNext())
-			{
-				oos.writeObject(it.next());
-			}
-			oos.close();
-			
 			/* Reading the diary */
 			File reader = new File("agenda.txt");
 			ObjectInputStream ois = new ObjectInputStream(
 					new BufferedInputStream(new FileInputStream(reader)));
-			
-			AbsEvent e;
-			while((e = (AbsEvent)(ois.readObject())) != null)
-			{
-				System.out.println("Event read : " + e);	
+
+			System.out.println("*********** From file ***********");
+			int lineToRead = (int)ois.readInt();
+			for(int i=0;i<lineToRead;i++) {
+				System.out.println(((AbsEvent)ois.readObject()).toString());
 			}
+			
 			ois.close();
 
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
-			
+
 		} catch (NoSuchPaddingException e) {
 			e.printStackTrace();
-			
+
 		} catch (IllegalBlockSizeException e) {
 			e.printStackTrace();
-			
+
 		} catch (BadPaddingException e) {
 			e.printStackTrace();
-			
+
 		} catch (InvalidKeyException e) {
 			e.printStackTrace();
-			
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
-			
+
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
-			
+
 		}
-		
-
-
 
 	}
 
